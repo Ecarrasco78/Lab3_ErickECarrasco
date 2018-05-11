5e692f9870312e7d83619e436b2c24976cefd06a
Ecarrasco <ecarrasco78@unitec.com>
2018-05-11 13:37:19 -0600

    Se creo un menu principal

---
+++ Lab3_ErickCarrasco/src/lab3_erickcarrasco/Lab3_ErickCarrasco.java
@@ -0,0 +1,20 @@
+package lab3_erickcarrasco;
+
+public class Lab3_ErickCarrasco {
+
+    public static void main(String[] args) {
+        System.out.println("***********");
+        System.out.println("Bienvenido");
+        System.out.println("");
+        int menu = -1;
+        while(menu != 3){
+            System.out.println("1/ Iniciar");
+            System.out.println("2/ Difficulty");
+            System.out.println("3/Salir");
+            if (menu ==1) {
+                
+            }
+        }
+    }
+    
+}
